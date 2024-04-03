b78cc55c0105d844835cabc842f8762a8a89c478
agus <agusmian18@hotmail.com>
2024-04-03 00:59:10 -0300

    Actualizacion Header y Footer v2.0

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,5 @@
 
+/*
 const navToggle = document.querySelector(".nav-toggle");
 const navMenuLinks = document.querySelector(".nav-links");
 
@@ -10,4 +11,25 @@ navToggle.addEventListener("click", () => {
     } else {
         navToggle.setAttribute("aria-label", "Abrir menu");
     }
-}) 
+}) */
+
+/*
+var hamburger = document.getElementById('Hamburger');
+var menu = document.getElementById('Menu');
+
+hamburger.addEventListener('click', function(){
+    menu.classList.toggle('open');
+}) */
+
+const toggleBtn = document.querySelector('.toggle_btn');
+const toggleBtnIcon = document.querySelector('.toggle_btn i');
+const dropDownMenu = document.querySelector('.dropdown_menu');
+
+toggleBtn.onclick = function() {
+    dropDownMenu.classList.toggle('open');
+    const isOpen = dropDownMenu.classList.contains('open');
+
+    toggleBtnIcon.classList = isOpen
+    ? 'fa-solid fa-xmark'
+    : 'fa-solid fa-bars'
+}
