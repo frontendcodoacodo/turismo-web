d3a84264b36050bd605800e66c19991bc1bc6d19
agus0418 <agusmian18@hotmail.com>
2024-04-30 18:57:27 -0300

    Update Header y responsive

--- js/header.js
+++ js/header.js
@@ -1,31 +1,29 @@
 const header = document.querySelector(".header");
 
 header.innerHTML = `
-  <div class="navbar">
-    <div class="logo">
+        <div class="logo">
         <a href="index.html"><img src="img/logo-turismo.png" alt="logo-viajes"></a>
-    </div>            
-        <ul class="navbar-links">
-        <li><a href="index.html">Inicio</a></li>                
-        <li><a href="#">Donde Ir</a></li> 
-        <li><a href="sobre_nosotros.html">Sobre Nosotros</a></li> 
-        <li><a href="contacto.html" class="nav-menu-link_active" >Contacto</a></li>   
+        </div>
+        <input type="checkbox" id="menu-bar"></input>
+        <label for="menu-bar"> <i class="fa-solid fa-bars"></i><h6>Menu</h6></label>
+        <div class="navbar">
+        <ul class="navbar-links"> 
+           <li><a href="index.html">Inicio</a></li>                
+           <li><a href="#">Donde Ir +</a>
+               <ul class="submenu">
+                    <li><a href="#">Mendoza</a></li>
+                    <li><a href="#">San Juan</a></li>
+                    <li><a href="tucuman.html">Tucuman</a></li>
+                   <li><a href="#">Corrientes</a></li>
+               </ul>
+           </li> 
+           <li><a href="sobre_nosotros.html">Sobre Nosotros</a></li> 
+           <li><a href="contacto.html" class="nav-menu-link_active" >Contacto</a></li>   
         </ul>                     
+        </div> 
+        <div class="buttons">
         <a href="#" class="action_btn">Consultanos</a>
-    <div class="toggle_btn">
-        <i class="fa-solid fa-bars"></i>
-    </div>
-    <div class="icon-whatsapp">
         <i class="fa-brands fa-whatsapp"></i> 
         <i class="fa-brands fa-facebook"></i>
-    <a href="#"></a>
-    </div>
-    </div> 
-    <div class="dropdown_menu">
-        <li><a href="index.html">Inicio</a></li>                
-        <li><a href="#">Donde Ir</a></li> 
-        <li><a href="sobre_nosotros.html">Sobre Nosotros</a></li> 
-        <li><a href="contacto.html" class="nav-menu-link_active" >Contacto</a></li>
-        <a href="#" class="action_btn">Consultanos</a>
-    </div>
+        </div>
 `;
